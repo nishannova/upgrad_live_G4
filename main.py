d805793e7c4806bfaad43063dd7e985eb42c467f
nishannova <nishan.ali495@gmail.com>
2022-04-30 10:30:46 +0530

    Revert "Addition of multiplication feature"
    This reverts commit 1ef06afa809a81970b90ca9a8785e0c07e55324f.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from sum_num import sum_
-from multiplication import mult
 
 if __name__=="__main__":
     x = input("Enter first Integer: ")
     y = input("Enter another Integer to be added: ")
     print("Summation Result: {}".format(sum_(x, y)))
-    print("Multiplication Result: {}".format(mult(x, y)))
--- multiplication.py
+++
@@ -1,9 +0,0 @@
-#This file is meant for multiplication
-
-def mult(x, y):
-    """
-    Params: x: Int
-    Params: y: Int
-    return: Summation of x & y
-    """    
-    return x * y
