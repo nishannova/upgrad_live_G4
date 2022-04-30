f9cd2d340f0f54db90fe0c30b9876720d1e7e483
nishannova <nishan.ali495@gmail.com>
2022-04-30 10:17:21 +0530

    Summation Function

---
+++ main.py
@@ -0,0 +1,6 @@
+from sum_num import sum_
+
+if __name__=="__main__":
+    x = input("Enter first Integer: ")
+    y = input("Enter another Integer to be added: ")
+    print("Summation Result: {}".format(sum_(x, y)))
---
+++ sum_num.py
@@ -0,0 +1,9 @@
+#This file is meant for summation
+
+def sum_(x, y):
+    """
+    Params: x: Int
+    Params: y: Int
+    return: Summation of x & y
+    """    
+    return x + y
