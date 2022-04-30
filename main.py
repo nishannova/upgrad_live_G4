eee9282aec6b40968d714826f65480f2574e5436
nishannova <nishan.ali495@gmail.com>
2022-04-30 10:35:34 +0530

    Addition of extra param

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__=="__main__":
     x = input("Enter first Integer: ")
     y = input("Enter another Integer to be added: ")
     print("Summation Result: {}".format(sum_(x, y)))
-    print("Multiplication Result: {}".format(mult(x, y)))
+    print("Multiplication Result: {}".format(mult(x, y, 10)))
