1ef06afa809a81970b90ca9a8785e0c07e55324f
nishannova <nishan.ali495@gmail.com>
2022-04-30 10:24:24 +0530

    Addition of multiplication feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sum_num import sum_
+from multiplication import mult
 
 if __name__=="__main__":
     x = input("Enter first Integer: ")
     y = input("Enter another Integer to be added: ")
     print("Summation Result: {}".format(sum_(x, y)))
+    print("Multiplication Result: {}".format(mult(x, y)))
---
+++ multiplication.py
@@ -0,0 +1,9 @@
+#This file is meant for multiplication
+
+def mult(x, y):
+    """
+    Params: x: Int
+    Params: y: Int
+    return: Summation of x & y
+    """    
+    return x * y
